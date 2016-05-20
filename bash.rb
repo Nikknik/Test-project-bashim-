90bcd19fe8f04db5e665109809a16555ff97e05f
Nikita <nnikitin82@gmail.com>
2016-05-21 00:35:17 +0300

    Adding id

--- bash.rb
+++ bash.rb
@@ -6,10 +6,10 @@ def bash
     require 'nokogiri'
     text = Nokogiri::HTML(html)
 
-    text.css('.text').each do |i|
-    	puts i.text.encode('utf-8')
-    	puts ""
-
+    text.css('.quote').each do |i|
+    	puts i.css('.id').text.encode('utf-8')
+        puts i.css('.text').text.encode('utf-8')
+        puts ""
     end
 end
 
