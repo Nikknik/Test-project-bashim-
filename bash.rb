7a415c5e95075381d6365687be49a385e7933041
Nikita <nnikitin82@gmail.com>
2016-05-23 05:18:05 +0300

    Adding comments

--- bash.rb
+++ bash.rb
@@ -1,14 +1,19 @@
 def bash
 
+    #получаем html сайта
 	require 'open-uri'
-    html=open('http://bash.im')
+    html=open('http://bash.im')  
 
     require 'nokogiri'
     text = Nokogiri::HTML(html)
 
+    #используем класс .quote для получения цитат
     text.css('.quote').each do |i|
+    	#вывод на экран id
     	puts i.css('.id').text.encode('utf-8')
+    	#вывод на экран самих цитат
         puts i.css('.text').text.encode('utf-8')
+        #пустая разделительная строка
         puts ""
     end
 end
